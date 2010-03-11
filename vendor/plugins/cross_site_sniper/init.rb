556c00b5026a0bf81f2a7672b809503fb52d7f66
Andrew Smith <fullsailor@me.com>
2010-03-10 20:40:20 -0500

    Removing vendored plugins.

--- vendor/plugins/cross_site_sniper/init.rb
+++
@@ -1,4 +0,0 @@
-# Include hook code here
-require 'cross_site_sniper'
-ActiveRecord::Base.class_eval { include ActiveRecord::AttributeMethods::ClassMethods }
-ActiveRecord::Base.class_eval { include ActiveRecord::CrossSiteSniperExtensions }
--- vendor/plugins/stripper/init.rb
+++
@@ -1,1 +0,0 @@
-require 'stripper' unless defined?(RedHillConsulting::Stripper)
