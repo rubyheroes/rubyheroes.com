21de104d0279032262a6abc5b4304c48c7b0c1dc
Andrew Smith <fullsailor@me.com>
2010-03-10 20:34:13 -0500

    Using Bundler to handle plugins

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ require "rubygems"
 alias require_gem gem
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
