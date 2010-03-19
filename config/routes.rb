97ec6194dfc126eac6b51c4af78aa9820b3e2aea
Andrew Smith <fullsailor@me.com>
2010-03-19 15:50:34 -0400

    New routes, removed duplicates.

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,15 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :sites
+  map.resources :sites, :collection => {:search=>:get}
 
   map.resources :heroes
 
-  map.resources :heros
-
-  map.resources :urls
-
   map.resources :nominators
-
-  map.resources :nominators
-
 
   map.resource :similar_nominations_search
 
-  map.resources :nominations
+  map.resources :nominations, :new => {:new=>:any}
   map.resources :nomination_sites
-
+  
   map.root :controller => "similar_nominations_searches", :action => "new"
 
   # Install the default routes as the lowest priority.
