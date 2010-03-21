c9e4c06247915612777d57a921e7513a8e395f5d
Andrew Smith <fullsailor@me.com>
2010-03-20 22:38:43 -0400

    Moved SimilarNominationsSearch functionality to Sites#search. Rerouted to match.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,12 +1,12 @@
 class SitesController < ApplicationController
   
   def search
-    if params[:site_url] && params[:site_url].length > 2
-      @sites = Site.all(:conditions=>['url LIKE ?',params[:site_url]+'%'])
+    if request.xhr? && params[:site_url] && params[:site_url].length > 2
+      @sites = Site.all(:conditions=>['url LIKE ?', params[:site_url]+'%'], :limit=>10)
+      render :action => "search_xhr", :layout => false
     else
       @sites = []
     end
-    render :layout => false
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :heroes, :nominators
   map.resources :nominations, :new => {:new => :any}
   map.resources :sites, :collection => {:search => :get}, :has_many=>:nominations
-
-  map.resource :similar_nominations_search
+  
   map.resources :nomination_sites
   
-  map.root :controller => "similar_nominations_searches", :action => "new"
+  map.root :controller => "sites", :action => "search"
 
   # Install the default routes as the lowest priority.
   # Leave 'em out, we're RESTful
