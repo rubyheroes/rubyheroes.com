b95702c32cbf9a659f44b8d0717a0d7b88c95049
Andrew Smith <fullsailor@me.com>
2010-03-24 22:47:23 -0400

    Validation message update.

--- app/models/nomination.rb
+++ app/models/nomination.rb
@@ -1,8 +1,8 @@
 class Nomination < ActiveRecord::Base
   belongs_to :nominator
   belongs_to :site
-  validates_length_of :testimonial, :minimum => 25, :message => "must be at least 25 charaters"
-  validates_presence_of :nominee,
+  validates_length_of :testimonial, :minimum => 25, :message => "Testimony must be at least 25 charaters."
+  validates_presence_of :nominee, :message => "This field is required."
   accepts_nested_attributes_for :nominator
   accepts_nested_attributes_for :site
 end
