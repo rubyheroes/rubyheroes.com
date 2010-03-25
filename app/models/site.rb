d318ad34160c61e4bb6e504730cfd5721b18dae4
Andrew Smith <fullsailor@me.com>
2010-03-24 20:26:37 -0400

    Strip off http:// on Site#url=

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,4 +2,15 @@ class Site < ActiveRecord::Base
   has_many :nominations
   attr_accessible :url, :name
   validates_presence_of :url, :name
+  
+  def url=(value)
+    value.strip
+    begin
+      uri= URI.parse(value)
+      write_attribute(:url, uri.host+uri.path)
+    rescue URI::InvalidURIError
+      errors.add(:url, "is an invalid url")
+    end
+  end
+  
 end
