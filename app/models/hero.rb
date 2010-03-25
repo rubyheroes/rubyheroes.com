762cf903a3834281f7424d0f68de3db327eabe51
Andrew Smith <fullsailor@me.com>
2010-03-25 00:03:35 -0400

    Table name fix.

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -2,7 +2,7 @@ class Hero < ActiveRecord::Base
   attr_accessible :name, :avatar, :year
   def self.find(*args)
     if args.first.to_s == "random"
-      ids = connection.select_all("SELECT id FROM heroes")
+      ids = connection.select_all("SELECT id FROM "+table_name)
       super(ids[rand(ids.length)]["id"].to_i)
     else
       super
