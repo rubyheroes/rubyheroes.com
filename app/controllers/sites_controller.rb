b1c429917a6e7f0d1e68c13912c91dba4a0ee97b
Andrew Smith <fullsailor@me.com>
2010-03-28 22:11:17 -0400

    Admin section at /sites

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,5 +1,4 @@
 class SitesController < ApplicationController
-  
   def search
     if request.xhr? && params[:site_url] && params[:site_url].length > 2
       @sites = Site.all(:conditions=>['url LIKE ?', params[:site_url]+'%'], :limit=>10)
@@ -10,9 +9,14 @@ class SitesController < ApplicationController
       end
     else
       session[:real_visit] = true
+      session[:ns] = false
       @sites = []
       @heroes = [Hero.find(:random),Hero.find(:random)]
     end
   end
-
+  
+  def index
+    @sites = Site.paginate(:order=>"nominations_count DESC", :page => params[:page])
+    render :index, :layout => 'admin'
+  end
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,7 +1,10 @@
 class Site < ActiveRecord::Base
+  
   has_many :nominations
   attr_accessible :url, :name
   validates_presence_of :url, :name, :message => "This field is required."
+  cattr_reader :per_page
+  @@per_page = 15
   
   def url=(value)
     return if value.nil?
@@ -9,5 +12,4 @@ class Site < ActiveRecord::Base
     value.gsub! /(https?:\/\/)/, ""
     self[:url] = value
   end
-  
 end
