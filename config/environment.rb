2a64a7e5ae0e975e02c8b477f05e1dbb18429ae5
Andrew Smith <fullsailor@me.com>
2010-03-28 22:10:14 -0400

    Added will_paginate

--- config/environment.rb
+++ config/environment.rb
@@ -19,9 +19,10 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "haml", :version => '>=2.2.22'
-  config.gem "rdiscount", :version => '>=1.6.3'
-  config.gem "formtastic", :version => '>=0.9.4'
+  config.gem "haml", :version => '~>2.2.22'
+  config.gem "rdiscount", :version => '~>1.6.3'
+  config.gem "formtastic", :version => '~>0.9.4'
+  config.gem "will_paginate", :version => '~>2.3.11'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
