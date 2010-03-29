c25938caf042882c617a633841e1888150d0cf56
Andrew Smith <fullsailor@me.com>
2010-03-29 03:27:19 -0400

    HeroesController rewrite.

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -4,10 +4,25 @@ class HeroesController < ApplicationController
     @year = params[:year]
     @heroes = Hero.find_all_by_year(@year)
     
-    if request.xhr?
-      render :action => "xhr/year", :layout => false and return
+    response.headers['Cache-Control'] = 'public, max-age=43200'
+    respond_to do |format|
+      format.html do
+        render :file => "#{RAILS_ROOT}/public/404.html", :status => 404 if @heroes.empty?
+      end
+      format.json { render :json => @heroes.to_json(:only => [:name, :url, :year]) }
     end
     
-    render :file => "#{RAILS_ROOT}/public/404.html", :status => 404 if @heroes.empty?
+  end
+  
+  def index
+    @heroes = Hero.all
+    
+    response.headers['Cache-Control'] = 'public, max-age=43200'
+    respond_to do |format|
+      format.html do
+        render :file => "#{RAILS_ROOT}/public/404.html", :status => 404 if @heroes.empty?
+      end
+      format.json { render :json => @heroes.to_json(:only => [:name, :url, :year]) }
+    end
   end
 end
