32cb1fe192f9f6e42fc9104aa9e66a4f168db40c
Andrew Smith <fullsailor@me.com>
2010-03-29 20:54:23 -0400

    Fixed the popdown mess.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,18 +6,12 @@ jQuery(function($){
     .keyup(function(event) {
       var input_text = $(event.target).val();
       if (input_text.length > 2) {
-        $('#yes_match').css('display','block').load('/sites/search?site_url=' + input_text);
+        $('#yes_match').css({display:"block"}).load('/sites/search?site_url=' + input_text, function(event) {$('#yes_match').append($('<span>'));});
       } else {
-        $('#yes_match').empty();
+        $('#yes_match').css({display:"none"}).empty();
       }
       $('#nomination_submit').html('Nominate <strong>' + input_text + '</strong>')
       return false;
     })
   ;
-  var heroes = $("#heroes");
-  var oheight = $("#y2008").appendTo(heroes);
-  var ohnine = $("#y2009").appendTo(heroes);
-  
-  oheight.load("/heroes/year?year=2008");
-  ohnine.load("/heroes/year?year=2009");
 });
