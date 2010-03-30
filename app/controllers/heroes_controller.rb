dc0643afc1d2829ee0cbbcda930e88ff8fdccba7
Andrew Smith <fullsailor@me.com>
2010-03-29 20:06:50 -0400

    Show current Nominees.

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -16,8 +16,9 @@ class HeroesController < ApplicationController
   
   def index
     @heroes = Hero.all
-    
-    response.headers['Cache-Control'] = 'public, max-age=43200'
+    @nominations = Nomination.all(:select=>"nominee", :group => "nominee")
+    @nominees = @nominations.map { |nomination| nomination.nominee }
+    response.headers['Cache-Control'] = 'public, max-age=300'
     respond_to do |format|
       format.html do
         render :file => "#{RAILS_ROOT}/public/404.html", :status => 404 if @heroes.empty?
