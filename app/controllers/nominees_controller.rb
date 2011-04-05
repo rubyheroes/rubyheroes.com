4830bfbde6873dbf8f957fd4496cb50b29be6176
Andrew  Smith <andrew@envylabs.com>
2011-04-05 13:42:24 -0400

    Style tweaks.

--- app/controllers/nominees_controller.rb
+++ app/controllers/nominees_controller.rb
@@ -2,7 +2,10 @@ class NomineesController < ApplicationController
 
   def index
     @nominees = Nominee.begins_with(params[:q]).limit(10)
-    render :index, :layout => false
+    if @nominees.any?
+      render :index, :layout => false
+    else
+      render :nothing => true
   end
 
 end
