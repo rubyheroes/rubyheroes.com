dd6b30579898e74438f5776a5ad089cbbb5d6e58
Olivier Lacan <olivier.lacan@gmail.com>
2012-01-18 11:46:08 -0500

    Added simple stats for nominations, nominees and voters in admin.

--- app/controllers/admin/nominees_controller.rb
+++ app/controllers/admin/nominees_controller.rb
@@ -2,6 +2,9 @@ module Admin
   class NomineesController < AdminController
 
     def index
+      @nomination_count = Nomination.count
+      @voter_count = Nominator.count
+      @nominee_count = Nominee.count
       @nominees = Nominee.order('nomination_count DESC').page(params[:page])
     end
     
