05ee79c4ab63a55561e6d87c1978cd3c09650511
Olivier Lacan <olivier.lacan@gmail.com>
2013-02-11 17:21:38 -0500

    Added a @past_heroes variable to home#show. Fixes #3.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
 
   def show
-    @heroes = Hero.where(:year => '2011')
+    @heroes = Hero.where(:year => '2012')
+    @past_heroes = Hero.where('year IS NOT 2012')
   end
 
 end
