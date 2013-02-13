6c3835a09f645aa031bf24c797c9090621f70624
Olivier Lacan <olivier.lacan@gmail.com>
2013-02-13 18:39:42 -0500

    Use the force of Arel tables, Luke.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def show
     @heroes = Hero.where(:year => '2012')
-    @past_heroes = Hero.where('year IS NOT 2012')
+    @past_heroes = Hero.where(Hero.arel_table[:year].not_eq(2012))
   end
 
 end
