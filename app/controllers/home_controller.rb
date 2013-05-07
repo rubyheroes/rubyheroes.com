5e221465f98c178cb4227ee372a1b6988a756139
Olivier Lacan <olivier.lacan@gmail.com>
2013-05-07 03:42:15 +0200

    Fix date integer instead of string.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def show
-    last_year = 2013
+    last_year = "2013"
     @heroes = Hero.where(:year => last_year)
     @past_heroes = Hero.where(Hero.arel_table[:year].not_eq(last_year)).group_by(&:year)
   end
