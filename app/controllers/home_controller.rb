d96fbf1784718b4cc29bc8b51398a371b68ccc4a
Olivier Lacan <olivier.lacan@gmail.com>
2013-05-07 03:57:20 +0200

    Fix hero year sorting.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,10 @@ class HomeController < ApplicationController
   def show
     last_year = "2013"
     @heroes = Hero.where(:year => last_year)
-    @past_heroes = Hero.where(Hero.arel_table[:year].not_eq(last_year)).group_by(&:year)
+    @past_heroes = Hero.where(Hero.arel_table[:year].
+      not_eq(last_year)).
+      group_by(&:year).
+      sort.reverse!
   end
 
 end
