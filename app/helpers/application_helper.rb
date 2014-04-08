5ff781d45f94174714bb2dc2167fd95b8a6cb753
Olivier Lacan <hi@olivierlacan.com>
2014-04-08 19:48:41 -0400

    Add new state when heroes have been chosen but not revealed.
    This is useful after voting has closed and before RailsConf.
    
    Also updates the Code School course link to the latest Rails course.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
+  # Making this boolean false will disable the voting interface on the home page.
   def voting_enabled?
     false
+  end
+
+  # Making this boolean true will link to the previous year's heroes.
+  def heroes_chosen?
+    true
   end
 end
