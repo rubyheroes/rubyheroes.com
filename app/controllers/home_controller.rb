3bea62783732856c700cc94c5b542d64452059e2
Olivier Lacan <hi@olivierlacan.com>
2015-03-11 11:18:45 -0400

    I write bugs, you write bugs.
    So shut up.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
 
   def show
-    last_year = "2015"
-    @heroes = Hero.where(:year => last_year)
+    @last_year = "2015"
+    @heroes = Hero.where(:year => @last_year)
     @past_heroes = Hero.where(Hero.arel_table[:year].
-      not_eq(last_year)).
+      not_eq(@last_year)).
       group_by(&:year).
       sort.reverse!
   end
