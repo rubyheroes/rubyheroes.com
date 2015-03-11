6dcb4bdc80fddb356f5a46df834e64615bc7fc99
Olivier Lacan <hi@olivierlacan.com>
2015-03-11 16:33:52 -0400

    Fix error due to last_year being incorrect.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
 
   def show
-    @last_year = "2015"
-    @heroes = Hero.where(:year => @last_year)
-    @past_heroes = Hero.where(Hero.arel_table[:year].
-      not_eq(@last_year)).
+    @this_year = "2015"
+    last_year = "2014"
+    @heroes = Hero.where(:year => last_year)
+    @past_heroes = Hero.where("year != ?", last_year).
       group_by(&:year).
       sort.reverse!
   end
