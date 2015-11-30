1f4348fedd974d56e77d61cc5a95882c1aa19536
Olivier Lacan <hi@olivierlacan.com>
2015-11-30 10:10:51 -0500

    Enable voting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Making this boolean false will disable the voting interface on the home page.
   def voting_enabled?
-    false
+    true
   end
 
   # Making this boolean true will link to the previous year's heroes.
