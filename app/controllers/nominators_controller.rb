1f22cddb5d98a537ec52e9e711e5db6ca1a761b5
Olivier Lacan <hi@olivierlacan.com>
2015-11-29 19:18:32 -0500

    Remove useless views & forms

--- app/controllers/nominators_controller.rb
+++ app/controllers/nominators_controller.rb
@@ -7,41 +7,6 @@ class NominatorsController < ApplicationController
     @nominator = Nominator.find(params[:id])
   end
 
-  def new
-    @nominator = Nominator.new
-  end
-
-  def create
-    @nominator = Nominator.new(nominator_params)
-    if @nominator.save
-      flash[:notice] = "Successfully created nominator."
-      redirect_to @nominator
-    else
-      render :action => 'new'
-    end
-  end
-
-  def edit
-    @nominator = Nominator.find(params[:id])
-  end
-
-  def update
-    @nominator = Nominator.find(params[:id])
-    if @nominator.update_attributes(nominator_params)
-      flash[:notice] = "Successfully updated nominator."
-      redirect_to @nominator
-    else
-      render :action => 'edit'
-    end
-  end
-
-  def destroy
-    @nominator = Nominator.find(params[:id])
-    @nominator.destroy
-    flash[:notice] = "Successfully destroyed nominator."
-    redirect_to nominators_url
-  end
-
   private
 
   def nominator_params
