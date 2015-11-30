b7298d57be8f0569cd6e1959061e7c515d1ef112
Olivier Lacan <hi@olivierlacan.com>
2015-11-30 10:09:51 -0500

    Nominees controller refactor

--- app/controllers/nominees_controller.rb
+++ app/controllers/nominees_controller.rb
@@ -1,17 +1,15 @@
 class NomineesController < ApplicationController
-
   def index
     @nominees = Nominee.begins_with(params[:q]).limit(3)
     if @nominees.any?
-      render :index, :layout => false
+      render :index, layout: false
     else
-      render :nothing => true
+      render nothing: true
     end
   end
-  
+
   def show
     @site = Site.find(params[:id])
-    render :show, :layout => 'admin'
+    render :show, layout: "admin"
   end
-
 end
