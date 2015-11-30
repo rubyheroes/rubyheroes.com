5720252f7aec8d91658655a394c8a291d081d917
Olivier Lacan <hi@olivierlacan.com>
2015-11-30 10:08:54 -0500

    Refactor Heroes controller

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,28 +1,20 @@
 class HeroesController < ApplicationController
-  
   def year
     @year = params[:year]
-    @heroes = Hero.find_all_by_year(@year)
-    
-    response.headers['Cache-Control'] = 'public, max-age=43200'
+    @heroes = Hero.where(year: @year)
+
     respond_to do |format|
-      format.html do
-        render :file => "#{RAILS_ROOT}/public/404.html", :status => 404 if @heroes.empty?
-      end
-      format.json { render :json => @heroes.to_json(:only => [:name, :url, :year]) }
+      format.html { render root_path(anchor: "heroes", year: @year) }
+      format.json { render json: @heroes.to_json(only: [:name, :url, :year]) }
     end
-    
   end
-  
+
   def index
-    @heroes = Hero.all
-    response.headers['Cache-Control'] = 'public, max-age=300'
     respond_to do |format|
-      format.html do
-        redirect_to root_path(anchor: "heroes")
-        # render :file => "#{RAILS_ROOT}/public/404.html", :status => 404 if @heroes.empty?
+      format.html { redirect_to root_path(anchor: "heroes") }
+      format.json do
+        render json: Hero.select(:name, :url, :year).to_json(only: [:name, :url, :year])
       end
-      format.json { render :json => @heroes.to_json(:only => [:name, :url, :year]) }
     end
   end
 end
