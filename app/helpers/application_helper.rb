9586b93479f6dd76cc47e9cde748a312eb260a87
Olivier Lacan <hi@olivierlacan.com>
2016-02-27 01:20:56 -0500

    Disable voting and reset heroes for 2016

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
   # Making this boolean false will disable the voting interface on the home page.
   def voting_enabled?
-    true
+    false
   end
 
   # Making this boolean true will link to the previous year's heroes.
   def heroes_chosen?
-    true
+    false
   end
 
   def gauges_tracker
