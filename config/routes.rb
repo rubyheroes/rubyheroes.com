3bde2b65a0c1b013b7a9e889f7f9c359be65fa60
Olivier Lacan <hi@olivierlacan.com>
2016-03-20 07:10:57 +0100

    Removed unused legacy views

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ RubyHeroAwards::Application.routes.draw do
   get '/.well-known/acme-challenge/:id' => 'pages#lets_encrypt'
 
   resources :nominations
+
   resources :heroes, only: [:index] do
     collection do
       get ":year", to: "heroes#year"
     end
   end
+
   resources :nominees, only: [:index]
 
   namespace :admin do
