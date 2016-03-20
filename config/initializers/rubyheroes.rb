4b6dcd7a67ec874ab74b0017addaa96c423283b7
Olivier Lacan <hi@olivierlacan.com>
2016-03-20 05:38:16 +0100

    Enable voting

--- config/initializers/rubyheroes.rb
+++ config/initializers/rubyheroes.rb
@@ -1,4 +1,4 @@
 # Making this boolean false will disable the voting interface on the home page.
-$voting_enabled = false
+$voting_enabled = true
 # Making this boolean true will link to the previous year's heroes.
 $heroes_chosen = false
