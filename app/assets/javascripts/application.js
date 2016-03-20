444bf88595e458e627cc83a428089a81d2e287df
Olivier Lacan <hi@olivierlacan.com>
2016-03-20 05:13:42 +0100

    Go to absurd length to enable arrow keys & Enter
    Seriously what was I even thinking. It brought me back to Full Sail days
    fiddling with jQuery this much but eventually I figured it out I think.
    
    The autocomplete suggestions are now navigable with arrow keys (can't
    wait to hear the Vim-ers complain :-p) and submittable with the Enter
    key which is a lot better.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,6 +6,85 @@
 //= require_self
 //= require_tree .
 
+var enterKey = 13;
+var upArrow = 38;
+var downArrow = 40;
+
+var focusSuggestions = function(event) {
+  console.log("focus")
+  var suggestions = $("#yes_match li");
+
+  // if we have any autocomplete matches
+  if (suggestions.length > 0) {
+
+    if (event.keyCode == upArrow) {
+      suggestions.children(":first").trigger("focus");
+
+      event.preventDefault();
+    } else if (event.keyCode == downArrow) {
+      suggestions.children(":last").trigger("focus");
+
+      event.preventDefault();
+    }
+  }
+};
+
+var interceptSubmit = function(event){
+  console.log("intercept")
+  if (event.keyCode == enterKey) {
+    submitSuggestion(event);
+  } else if (event.keyCode == upArrow || event.keyCode == downArrow) {
+    cycleThroughSuggestions(event);
+  }
+}
+
+var cycleThroughSuggestions = function(event) {
+  console.log("cycle")
+
+  var suggestions = $("#yes_match li");
+  var firstSuggestion = suggestions.children(":first")[0];
+  var lastSuggestion = suggestions.children(":last")[0];
+
+  var nextSuggestion = function(currentSuggestion) {
+    return $(currentSuggestion).parent().next().find("a")[0];
+  }
+
+  var previousSuggestion = function(currentSuggestion) {
+    return $(currentSuggestion).parent().prev().find("a")[0];
+  }
+
+  currentSuggestion = event.currentTarget;
+
+  if (event.keyCode == upArrow) {
+    if (currentSuggestion == firstSuggestion) {
+      $(lastSuggestion).trigger("focus");
+    } else {
+      $(previousSuggestion(currentSuggestion)).trigger("focus")
+    };
+
+    event.preventDefault();
+  } else if (event.keyCode == downArrow) {
+    if (currentSuggestion == lastSuggestion) {
+      $(firstSuggestion).trigger("focus");
+    } else {
+      $(nextSuggestion(currentSuggestion)).trigger("focus")
+    };
+
+    event.preventDefault();
+  }
+};
+
+var submitSuggestion = function(event) {
+  console.log("submit")
+  var enterKey = 13;
+
+  if (event.keyCode == enterKey) {
+    event.currentTarget.trigger("click");
+
+    event.preventDefault();
+  }
+}
+
 jQuery(function($){
 
   // ----- Parse Heroes Hash ----- //
@@ -94,9 +173,7 @@ jQuery(function($){
 
         if (input_text.length > 2 ) {
           $(matches)
-            .load('/nominees/?q=' + input_text, function(event) {
-              $(matches).append($('<span>'));
-            })
+            .load('/nominees/?q=' + input_text)
             .show()
           $('.nominate').addClass('is-nominating');
         } else {
@@ -108,10 +185,26 @@ jQuery(function($){
       return false;
     });
 
+    // intercept the keydown events (arrow up and down only) on the nominee
+    // input field in order to redirect focus on either the first or the last
+    // available autocomplete suggestion
+    $("#nominee").on("keydown", focusSuggestions);
+
+    // intercept the keydown events on the autocomplete suggestion result links
+    // in order to cycle through them or submit them
+    $("body").on("keydown", "#results a", interceptSubmit);
+
     $("#vote form").submit( function(){
       var input_text = $("input#nominee").val();
       if (  regex1.test(input_text) || regex2.test(input_text) ) {
         return false;
       }
     });
+
+  // ----- Nomination Page ----- //
+  $("#new_nomination textarea").on("keydown", function(event){
+    if(event.keyCode == 13 && event.metaKey) {
+      $(event.currentTarget).parents("form:first").submit();
+    }
+  })
 });
