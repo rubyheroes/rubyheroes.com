a7e3f10384da706cfa5935ebf7a8c382eae84d37
Olivier Lacan <hi@olivierlacan.com>
2016-03-20 08:27:00 +0100

    Convert heroes year from string to integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320061822) do
+ActiveRecord::Schema.define(version: 20160320072348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20160320061822) do
   create_table "heroes", force: :cascade do |t|
     t.string   "name"
     t.string   "avatar"
-    t.string   "year"
+    t.integer  "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "url"
