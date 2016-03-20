ba0f4c8a04fb56e986a91f6646dcf2a6027791ea
Olivier Lacan <hi@olivierlacan.com>
2016-03-20 05:11:07 +0100

    Don't even try to validate email format
    What's the point.

--- app/models/nominator.rb
+++ app/models/nominator.rb
@@ -1,10 +1,6 @@
 class Nominator < ActiveRecord::Base
   has_many :nominations, :dependent => :destroy
 
-
-  validates_format_of :email,
-    with: /\A([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})\z/i,
-    message: "A valid email address is required."
   validates_presence_of :name,
     message: "This field is required."
   validates_presence_of :email,
