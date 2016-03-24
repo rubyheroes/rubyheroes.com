dfda7791c5c6ef5b721af47fd337de0e867577f1
Olivier Lacan <hi@olivierlacan.com>
2016-03-24 11:21:18 +0100

    Improve Nominee#merge_into examples a bit

--- spec/factories/nominee.rb
+++ spec/factories/nominee.rb
@@ -2,5 +2,11 @@ FactoryGirl.define do
   factory :nominee do
     name "Julie Ann Lovelace"
     github_username "julie_ann_lovelace"
+
+    trait :with_nomination do
+      after(:create) do |nominee|
+        nominee.nominations << create(:nomination)
+      end
+    end
   end
 end
--- spec/models/nominee_spec.rb
+++ spec/models/nominee_spec.rb
@@ -5,22 +5,28 @@ RSpec.describe Nominee do
 
   context "#merge_into(nominee)" do
     let!(:nomination) { create(:nomination, nominee: nominee) }
-    let!(:new_nominee) { create(:nominee, name: "Peter", github_username: "peter") }
+    let!(:new_nominee) { create(:nominee, :with_nomination, name: "Peter", github_username: "peter") }
 
     subject(:merge){ nominee.merge_into(new_nominee) }
 
     it "destroys the instance & switches its nominations to the passed nominee" do
+      nominee_nomination_count = nominee.nominations.count
+      new_nominee_nomination_count = new_nominee.nominations.count
+
       expect(merge).to be_truthy
       expect(nominee).to_not be_persisted
-      expect(nominee.nominations).to include(nomination)
+      expect(new_nominee.nominations).to include(nomination)
+      expect(new_nominee.nominations.count).to eq(nominee_nomination_count + new_nominee_nomination_count)
     end
 
     context "when attempting to merge a nominee into itself" do
       let!(:new_nominee) { nominee }
+      let!(:nomination) { create(:nomination, nominee: nominee) }
 
       it "not possible to merge into oneself" do
         expect(merge).to be_falsey
         expect(nominee).to be_persisted
+        expect(nomination).to be_persisted
       end
     end
   end
