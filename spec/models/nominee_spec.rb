9c701571e7c102d6b8804c8a6ac00a175c91bd72
Olivier Lacan <hi@olivierlacan.com>
2016-03-24 11:11:15 +0100

    Add example for @ symbol coercion

--- spec/models/nominee_spec.rb
+++ spec/models/nominee_spec.rb
@@ -1,10 +1,11 @@
 require "rails_helper"
 
 RSpec.describe Nominee do
+  let(:nominee) { create(:nominee) }
+
   context "#merge_into(nominee)" do
-    let(:nominee) { create :nominee }
-    let!(:nomination) { create :nomination, nominee: nominee }
-    let!(:new_nominee) { create :nominee, name: "Peter", github_username: "peter" }
+    let!(:nomination) { create(:nomination, nominee: nominee) }
+    let!(:new_nominee) { create(:nominee, name: "Peter", github_username: "peter") }
 
     subject(:merge){ nominee.merge_into(new_nominee) }
 
@@ -21,6 +22,16 @@ RSpec.describe Nominee do
         expect(merge).to be_falsey
         expect(nominee).to be_persisted
       end
+    end
+  end
+
+  context "#github_username" do
+    before { nominee.github_username = "@whatever" }
+
+    subject(:github_username){ nominee.github_username }
+
+    it "can't start with an @ symbol" do
+      expect(github_username).not_to include("@")
     end
   end
 end
