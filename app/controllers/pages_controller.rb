2c3007d4911128d18223851e612e9a4a3653e436
Olivier Lacan <hi@olivierlacan.com>
2016-03-28 23:08:53 +0200

    Try again to satisfy Let's Encrypt

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def lets_encrypt
-    render text: "OGduRFlfbe7KsyxczqfD33RO_W0FWGRJjTojKlM1mos.CRFFg2ylHX-dAS-TAtyvUkUwQ1tYenjudYtSmwAXA2U"
+    render text: "ugggnz2Lp05HgA31taCXKBev_KC9MALDL-EvkBisuqY.CRFFg2ylHX-dAS-TAtyvUkUwQ1tYenjudYtSmwAXA2U"
   end
 end
