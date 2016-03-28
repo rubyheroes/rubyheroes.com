ea489753b4555f48ee71d5df0ab45e51f457cd6b
Olivier Lacan <hi@olivierlacan.com>
2016-03-28 23:16:42 +0200

    Use the param, Luke

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def lets_encrypt
-    render text: "ugggnz2Lp05HgA31taCXKBev_KC9MALDL-EvkBisuqY.CRFFg2ylHX-dAS-TAtyvUkUwQ1tYenjudYtSmwAXA2U"
+    render text: params[:id]
   end
 end
