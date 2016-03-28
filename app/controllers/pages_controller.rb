dd7b4c8a4ca086f16f9c8c6600f29ea9f9275574
Olivier Lacan <hi@olivierlacan.com>
2016-03-28 23:25:03 +0200

    Guess it's going to be the hard way.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def lets_encrypt
-    render text: params[:id]
+    challenge = params[:id]
+
+    if challenge == "fSK3rze7XEjZ61BbT_kVRqCbrHUvQljxdsUEUklCW0A"
+      render text: "fSK3rze7XEjZ61BbT_kVRqCbrHUvQljxdsUEUklCW0A.CRFFg2ylHX-dAS-TAtyvUkUwQ1tYenjudYtSmwAXA2U"
+    end
   end
 end
