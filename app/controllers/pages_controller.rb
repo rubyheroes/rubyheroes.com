c1ec9dfcf91f69f6e115e202573797336042092c
Olivier Lacan <hi@olivierlacan.com>
2016-03-28 23:01:41 +0200

    Update Let's Encrypt challenge for new cert

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def lets_encrypt
-    render text: "AKFAI98WBpaPHsstsfv_DquKRLnk41-EkxuoQZiMlGU.CRFFg2ylHX-dAS-TAtyvUkUwQ1tYenjudYtSmwAXA2U"
+    render text: "OGduRFlfbe7KsyxczqfD33RO_W0FWGRJjTojKlM1mos.CRFFg2ylHX-dAS-TAtyvUkUwQ1tYenjudYtSmwAXA2U"
   end
 end
