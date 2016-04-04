6e9927ffe116c8e72a2c6e73a02df49e681190b7
Olivier Lacan <hi@olivierlacan.com>
2016-04-04 13:37:07 -0400

    Use IDs over usernames in admin
    In case of duplicate nominees we want to use IDs to merge
    them and not the potentially identical usernames.

--- app/controllers/admin/nominees_controller.rb
+++ app/controllers/admin/nominees_controller.rb
@@ -10,21 +10,21 @@ class Admin::NomineesController < Admin::AdminController
   end
 
   def show
-    @nominee = Nominee.find_by(github_username: params[:id])
+    @nominee = Nominee.find(params[:id])
     @recent_nominations = @nominee.recent_nominations.order(created_at: :desc)
     @nominations = @nominee.nominations.order(created_at: :desc)
   end
 
   def merge
-    @nominee = Nominee.find_by(github_username: params[:nominee_id])
-    @target_nominee = Nominee.find_by(github_username: params[:target_nominee])
+    @nominee = Nominee.find(params[:nominee_id])
+    @target_nominee = Nominee.find(params[:target_nominee_id])
 
     if @target_nominee && @nominee.merge_into(@target_nominee)
       flash[:notice] = "Successfully merged #{@nominee} into #{@target_nominee}."
-      redirect_to [:admin, @target_nominee]
+      redirect_to admin_nominee_path(@target_nominee.id)
     else
       flash[:notice] = "Failed to merge #{@nominee} into #{@target_nominee}."
-      redirect_to [:admin, @nominee]
+      redirect_to admin_nominee_path(@nominee.id)
     end
   end
 
