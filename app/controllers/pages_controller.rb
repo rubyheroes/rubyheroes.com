4008fd85ea21f5ca4212db59fbe648af5377187d
Olivier Lacan <hi@olivierlacan.com>
2016-07-02 16:22:04 -0400

    Update Let's Encrypt challenge

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   def lets_encrypt
     challenge = params[:id]
 
-    if challenge == "Da_8Nqje3-if5YSUmQNGSa3aR2wbE1VpVdr-CcvY-E4"
-      render text: "Da_8Nqje3-if5YSUmQNGSa3aR2wbE1VpVdr-CcvY-E4.CRFFg2ylHX-dAS-TAtyvUkUwQ1tYenjudYtSmwAXA2U"
+    if challenge == "Mc-emxHWYa_wjabUPkcmXdLOaEeAvZA6bRgUmG1DkGg"
+      render text: "Mc-emxHWYa_wjabUPkcmXdLOaEeAvZA6bRgUmG1DkGg.CRFFg2ylHX-dAS-TAtyvUkUwQ1tYenjudYtSmwAXA2U"
     elsif challenge == "m8t_Qv3hlIdussgiQdV3C6GyvkrfMwgXNMbhdVnkj9w"
       render text: "m8t_Qv3hlIdussgiQdV3C6GyvkrfMwgXNMbhdVnkj9w.CRFFg2ylHX-dAS-TAtyvUkUwQ1tYenjudYtSmwAXA2U"
     end
