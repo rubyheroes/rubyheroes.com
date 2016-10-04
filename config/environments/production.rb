39f0786a596652a1633efd204c6f72edd610f1d4
Olivier Lacan <hi@olivierlacan.com>
2016-10-04 04:45:51 +0200

    Set up automated Let’s Encrypt renewal

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # https://github.com/pixielabs/letsencrypt-rails-heroku#installation
+  config.middleware.insert_before ActionDispatch::SSL, Letsencrypt::Middleware
 end
